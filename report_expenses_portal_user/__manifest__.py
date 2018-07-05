60b9941475797ca717327c8842bc49c1077d86a8
oggonzalezm <ogm@soluciones4g.com>
2018-07-05 14:14:01 -0500

    [ADD] Report Expenses: Adding all the files(first commit)

---
+++ report_expenses_portal_user/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Report expenses from website",
+
+    'summary': 
+        """
+            Report expenses form website with a portal user
+        """,
+
+    'description':
+        """
+            A employee has a portal user, this user can report expenses it is related to a project.
+        """,
+
+    'author': "Soluciones4G",
+    'website': "",
+
+    'category': 'Extra Tools',
+    'version': '1.0',
+
+    'depends': [
+    ],
+
+    'demo': [],
+
+    'data': [
+
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

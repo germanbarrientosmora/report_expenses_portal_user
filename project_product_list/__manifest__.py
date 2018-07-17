ceacd4f5de4c55cac34c250d03a7cb9f28c526b2
ogm <cas_19996@hotmail.com>
2018-07-17 17:11:46 +0000

    [REF] project_product_list: modified manifest

--- project_product_list/__manifest__.py
+++ project_product_list/__manifest__.py
@@ -6,6 +6,7 @@
     """,
     'author': "Soluciones4G",
     'website': "http://www.soluciones4g.com",
+    'category': 'Extra Tools',
     'version': '0.1',
     'depends': ['base','product','project','purchase','stock'],
     'data': [
